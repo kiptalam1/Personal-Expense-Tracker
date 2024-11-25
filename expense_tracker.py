413642f1d1c3441f41bdedf1268a8bdc1d5d955f
adams <adamskiptalam0@gmail.com>
2024-11-25 23:06:32 +0300

    Add functionality to view expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import json
+import pandas as pd
+
 
 DATA_FILE = 'expenses.json'
 
@@ -10,27 +12,41 @@ def load_expenses():
     except FileNotFoundError:
         return []
 
-expenses = []
 
 def save_expenses(expenses):
     with open(DATA_FILE, 'w') as file:
         json.dump(expenses, file, indent=4)
 
+expenses = load_expenses()
+
 def add_expense():
     category = input('Enter Category: ').strip()
     amount = float(input('Enter the amount: '))
     date = input('Enter the date (YYYY-MM-DD) or press enter for today: ').strip()
-    
     # enter today's date if not provided.
     date = date if date else datetime.today().strftime("%Y-%m-%d")
     
-    expenses.append({'category': category, 'amount': amount, 'date': date})
-        
-    print("Expenses added successfully.")
+    if category:    
+        expenses.append({'category': category, 'amount': amount, 'date': date})
+        save_expenses(expenses)
+        print("Expenses added successfully.")
+    else:
+        return
     
-load_expenses()
-print(expenses)
 
+def view_expenses(expenses):
+    if not expenses:
+        print("No expenses to display.")
+        return
+    else:
+        df = pd.DataFrame(DATA_FILE)
+        print("\nExpenses:")
+        return df
+    
+    
+    
+add_expense()
+print(view_expenses())
 
 
 
